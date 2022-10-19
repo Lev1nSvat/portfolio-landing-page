8437d5762c872ae80b3aaddf49f2a6622405e1e4
Lev1nSvat <89538630+Lev1nSvat@users.noreply.github.com>
2022-10-19 15:13:48 +0300

    intro is responsive

--- pages/index.js
+++ pages/index.js
@@ -17,14 +17,15 @@ export default function Home({el, q, scrollIsLoaded, locoScroll}) {
   useEffect(() => {
     if(scrollIsLoaded) {
       const loading = gsap.timeline()
-      loading.fromTo(q('#gr1'), {attr:{offset: 0.7}}, {attr:{offset: 0.367}, duration: 3, ease: "power4"})
-      loading.set("#scrollBlock", {display: "none", background: "red"}, ">-0.2")
-      loading.set(q('#gr'), {attr:{y1:0.5, y2:0.5,x1:1,x2:0}})
-      loading.from(q('#scroll'), {y: "+=4vh", opacity: 0, duration: 1.2}, "<1.8")
+      let filling = loading.fromTo(q('#gr1'), {attr:{offset: "0.63"}}, {attr:{offset: 0.63 - window.innerWidth * 0.13 / window.innerHeight }, duration: 5, ease: "power4"})
+      loading.set("#scrollBlock", {display: "none", background: "red", onComplete:()=>{filling.kill()}}, ">-3")
+      loading.set(q("#gr1"), {attr:{offset:0}},">")
+      loading.set(q('#gr'), {attr:{y1:0.5, y2:0.5,x1:1,x2:0}},"<")
+      loading.from(q('#scroll'), {y: "+=4vh", opacity: 0, duration: 1.2}, ">")
       
       const intro = gsap.timeline({scrollTrigger: {scrub:true, pin:q('#intro'), end: 4000,}});
       intro.set(q("#gr1"), {attr:{offset:0}})
-      intro.from(q('#Sviatoslav'), {letterSpacing: "47vw", x:"7.5%", ease: "power4"} )
+      intro.from(q('#Sviatoslav'), {letterSpacing: "47vw", x:"8%", ease: "power4"} )
       intro.to(q("#intro"), {background: "linear-gradient(to right, #222222 0%, #FEE3EC 0%)"}, "<")
       intro.to(q("#gr1"), {attr:{offset: 1}}, "<")
       intro.fromTo(q("#surname"), {x: "100vw"}, {x: "-100vw", duration: 1.5,ease:"none"})
@@ -77,16 +78,19 @@ export default function Home({el, q, scrollIsLoaded, locoScroll}) {
       </Head>
       <div id="intro" className=" font-regular min-h-[100vh] bg-shark-500 bg-gr flex justify-center items-center">
         <svg className="h-[100vh] w-[100vw] flex justify-center items-center" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <mask id="mask">
-            <text id="Sviatoslav" className="text-[16vw] font-regular" fill="white" stroke="#FEE3EC">
-              <tspan x="4%" y="63%" className="">Sviatoslav</tspan>
+          <mask  id="mask">
+            <text id="Sviatoslav" className=" text-[16vw] translate-y-[63vh] translate-x-[10vw] font-regular h-[16vw] w-[100vw]" fill="white"  stroke="#FEE3EC">
+              <tspan className="">Sviatoslav</tspan>
             </text>
           </mask>
-          <rect width="100%" height="100%" fill="url(#gr)" mask="url(#mask)" ></rect>
-          <text id="Sviatoslav" strokeDasharray="100% 0%" strokeDashoffset="0%" className="name text-[16vw]" fill="none"  stroke="#FEE3EC">
-              <tspan x="4%" y="63%" className="">Sviatoslav</tspan>
+          <rect width="100%" height="100%" className="w-full h-[100vh]" fill="url(#gr)" mask="url(#mask)"></rect>  
+          <text id="Sviatoslav" strokeDasharray="100% 0%" strokeDashoffset="0%" className="translate-y-[63vh] translate-x-[10vw] name text-[16vw]" fill="none"  stroke="#FEE3EC">
+              <tspan className="">Sviatoslav</tspan>
           </text>
-          <text id="dev" strokeDasharray="0% 100%" className=" text-[14vw]" fill="none"  stroke="#FEE3EC">
+          <text id="surname" className="fill-carousel-pink-500 translate-y-[63vh] translate-x-[10vw] name text-[16vw]" fill="none" stroke="#FEE3EC">
+              <tspan className="">Monakhov</tspan>
+          </text>
+          <text id="dev" strokeDasharray="0% 100%" className=" text-[16vw]" fill="none"  stroke="#FEE3EC">
               <tspan x="6%" y="40%" className="dev hidden">Interactive</tspan>
               <tspan x="6%" y="70%" className="dev text-[15vw] hidden">Developer</tspan>
           </text>
@@ -98,7 +102,6 @@ export default function Home({el, q, scrollIsLoaded, locoScroll}) {
             </linearGradient>
           </defs>
         </svg>
-        <p id="surname" className="absolute text-[16vw] text-carousel-pink-500 ">Monakhov</p>
         <p id="scroll" className="text-carousel-pink-500 text-[2vw] absolute translate-y-[40vh]">scroll</p>
       </div>
       <div id="main" className="bg-carousel-pink-500 font-regular -z-10 relative min-h-[250vh] flex justify-between items-center">
