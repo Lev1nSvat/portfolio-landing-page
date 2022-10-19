eb12e07500e9551b3b8e764ad715253bff38b907
Lev1nSvat <89538630+Lev1nSvat@users.noreply.github.com>
2022-10-19 13:42:28 +0300

    some polishing

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home({el, q, scrollIsLoaded, locoScroll}) {
       main.to(q('#image'), {y:"+=250vh", ease:"none"})
 
       const main2 = gsap.timeline({scrollTrigger: {scrub:true, start:"top bottom", end:"bottom bottom", trigger:q('#main2')}})
-      main2.to(q('#about'), {y: "+=102vw",ease:"none"})
+      main2.to(q('#about'), {y: "+=350vh",ease:"none"})
 
       gsap.to(q('#link1'), {backgroundImage: "linear-gradient(to right, #222222 0%, #FEE3EC 0%)"})
       gsap.utils.toArray(q('.projects')).forEach(a => {
@@ -139,12 +139,13 @@ export default function Home({el, q, scrollIsLoaded, locoScroll}) {
       <div id="main2" className="relative z-30 bg-shark-500 overflow-hidden">
         <p id="about" className="text-[30vw] origin-top-left -rotate-90 w-fit absolute   font-regular text-carousel-pink-500">About</p>
         <div className="w-[75vw] ml-[30vw] text-5xl py-[30vh] skewElem px-[10vw] text-carousel-pink-500">
-          <p className="reveal py-12" >Hi, I'm Sviatoslav Monakhov, Interactive UI/UX developer.</p>
-          <p className="reveal py-12" >I'm currently offerring my expertise to agencies and creative teams.</p>
-          <p className="reveal py-12" >My love for challenge, makes me seek it in my work every day. Achieving today what wasn't possible for me yestarday is my passion. I hope I will find a team which will provide great oportunity to do so.</p>
-          <p className="reveal pt-72" >Feel free to say hello:</p>
+          <p className="reveal py-8" >Hi, I'm Sviatoslav Monakhov, Interactive UI/UX developer.</p>
+          <p className="reveal py-8" >I'm currently offerring my expertise to agencies and creative teams.</p>
+          <p className="reveal py-8" >My love for challenge, makes me seek it in my work every day. Achieving today what wasn't possible for me yestarday is my passion. I hope I will find a team which will provide great oportunity to do so.</p>
+          <p className="reveal py-8" >I'm a developer with taste for design, polished animation and big typography :)</p>
+          <p className="reveal pt-56" >Feel free to say hello:</p>
           <p className="reveal pb-4" ><a href="mailto:sviatoslavmonakhov@gmail.com">sviatoslavmonakhov@gmail.com</a></p>
-          <p className="reveal h-4  bg-carousel-pink-500" ></p>
+          <p className="reveal h-3  bg-carousel-pink-500" ></p>
           <p className="reveal pt-6 flex justify-end" ><a href="https://github.com/Lev1nSvat">  
             <svg className="fill-carousel-pink-500 h-16 w-16" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 32 32" width="32px" height="32px" ><path d="M 16 4 C 9.371094 4 4 9.371094 4 16 C 4 21.300781 7.4375 25.800781 12.207031 27.386719 C 12.808594 27.496094 13.027344 27.128906 13.027344 26.808594 C 13.027344 26.523438 13.015625 25.769531 13.011719 24.769531 C 9.671875 25.492188 8.96875 23.160156 8.96875 23.160156 C 8.421875 21.773438 7.636719 21.402344 7.636719 21.402344 C 6.546875 20.660156 7.71875 20.675781 7.71875 20.675781 C 8.921875 20.761719 9.554688 21.910156 9.554688 21.910156 C 10.625 23.746094 12.363281 23.214844 13.046875 22.910156 C 13.15625 22.132813 13.46875 21.605469 13.808594 21.304688 C 11.144531 21.003906 8.34375 19.972656 8.34375 15.375 C 8.34375 14.0625 8.8125 12.992188 9.578125 12.152344 C 9.457031 11.851563 9.042969 10.628906 9.695313 8.976563 C 9.695313 8.976563 10.703125 8.65625 12.996094 10.207031 C 13.953125 9.941406 14.980469 9.808594 16 9.804688 C 17.019531 9.808594 18.046875 9.941406 19.003906 10.207031 C 21.296875 8.65625 22.300781 8.976563 22.300781 8.976563 C 22.957031 10.628906 22.546875 11.851563 22.421875 12.152344 C 23.191406 12.992188 23.652344 14.0625 23.652344 15.375 C 23.652344 19.984375 20.847656 20.996094 18.175781 21.296875 C 18.605469 21.664063 18.988281 22.398438 18.988281 23.515625 C 18.988281 25.121094 18.976563 26.414063 18.976563 26.808594 C 18.976563 27.128906 19.191406 27.503906 19.800781 27.386719 C 24.566406 25.796875 28 21.300781 28 16 C 28 9.371094 22.628906 4 16 4 Z"/></svg>
           </a></p>
